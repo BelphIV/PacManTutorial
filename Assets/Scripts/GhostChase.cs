4d92db98a1d3c46538b0f4baed50f53a62476f68
BelphIV <79217206+BelphIV@users.noreply.github.com>
2023-03-20 03:08:52 +0300

    v0.7

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class GhostChase : GhostBehavior
 {
+    private void OnDisable()
+    {
+        this.ghost.scatter.Enable();
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+
+        if (node != null && this.enabled && !this.ghost.frightened.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+        }
+
+    }
 }
